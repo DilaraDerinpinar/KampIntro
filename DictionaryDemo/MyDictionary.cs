0c55103a2a920233e95e5ff89cd65a56d76e00b4
Dilara Derinpınar <dilaraderinpinar1@gmail.com>
2021-02-14 16:52:42 +0300

    14.02.2021

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<X>
+    {
+         X[] items;
+
+         public MyDictionary()
+         {
+             items = new X[0];
+         }
+
+         public void Add(X item)
+         {
+             X[] tempArray = items;
+             items = new X[items.Length + 1];
+             for (int i = 0; i < tempArray.Length; i++)
+             {
+                 items[i] = tempArray[i];
+             }
+             items[items.Length - 1] = item;
+
+        }
+
+         public int Length
+         {
+             get { return items.Length; }
+         }
+         public X[] Items
+         {
+             get { return items; }
+         }
+
+
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("sebnem");
+            Console.WriteLine(myDictionary.Length);
+            myDictionary.Add("hasan");
+            Console.WriteLine(myDictionary.Length);
+            myDictionary.Add("ezgi");
+            Console.WriteLine(myDictionary.Length);
+
+            foreach (var isim in myDictionary.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+        }
+    }
+    
+}
